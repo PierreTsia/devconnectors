92e7265d38f93127fa61c57cc558df9fea10f285
Pierre_T <pierre.tsiakkaros@gmail.com>
2018-09-08 16:52:41 +0200

    profile creds education

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -6,6 +6,35 @@ class ProfileCreds extends Component {
   render() {
     const { profile } = this.props;
 
+    let educationContent = profile.education.length ? (
+      profile.education.sort((a, b) => (a.from > b.from ? -1 : 1)).map(edu => {
+        return (
+          <li key={edu._id} className="list-group-item">
+            <h4>{edu.school}</h4>
+            <Moment format="DD/MM/YYYY">{edu.from}</Moment> -{" "}
+            {isEmpty(edu.to) ? (
+              <span>Now</span>
+            ) : (
+              <Moment format="DD/MM/YYYY">{edu.to}</Moment>
+            )}
+            {edu.degree ? (
+              <p>
+                {" "}
+                <strong>Degree:</strong> {edu.degree}
+              </p>
+            ) : null}
+            <p>
+              <strong>Field of Study:</strong> {edu.fieldofstudy}
+            </p>
+          </li>
+        );
+      })
+    ) : (
+      <li className="list-group-item">
+        <p> ...</p>
+      </li>
+    );
+
     let experienceContent = profile.experience.length ? (
       <ul className="list-group">
         {profile.experience
@@ -16,7 +45,7 @@ class ProfileCreds extends Component {
                 <h4>{exp.company}</h4>
                 <Moment format="DD/MM/YYYY">{exp.from}</Moment> -{" "}
                 {isEmpty(exp.to) ? (
-                  <span>current</span>
+                  <span>Now</span>
                 ) : (
                   <Moment format="DD/MM/YYYY">{exp.to}</Moment>
                 )}
@@ -45,7 +74,7 @@ class ProfileCreds extends Component {
 
           <div className="col-md-6">
             <h3 className="text-center text-info">Education</h3>
-            {experienceContent}
+            {educationContent}
           </div>
         </div>
       </div>
